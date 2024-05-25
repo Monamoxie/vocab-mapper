8b1f14ce8c3fdd923c5144c68a8aa73275b78664
Mona Moxie <moxie4lyf@gmail.com>
2024-05-25 14:30:24 +0200

    Bugfix: Fix database path for vendor publishhing

--- src/Traits/VocabHelper.php
+++ src/Traits/VocabHelper.php
@@ -6,13 +6,13 @@ trait VocabHelper
 {
     public function getMigrationDirectory(): string
     {
-        $main = database_path('/database/migrations/');
+        $main = database_path('/migrations');
         $subDir = config('vocab.migration_sub_dir', '');
 
         if (!empty($subDir)) {
             $subDir = ltrim(rtrim($subDir, "/"), "/");
         }
 
-        return sprintf('%s%s', $main, $subDir);
+        return sprintf('%s/%s', $main, $subDir);
     }
 }
